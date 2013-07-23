eb48e25dc5aa2126caa03b2cccc36ef6ad271625
ccyphers1 <ccyphers1@wccnet.edu>
2013-07-22 22:24:52 -0400

    Initial structure for the apply now page

--- ITintheDWebsite/ITintheDWebsite/Controllers/HomeController.cs
+++ ITintheDWebsite/ITintheDWebsite/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ITintheDWebsite.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace ITintheDWebsite.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDBContext db = new ApplicationDBContext();
+
         public ActionResult Index()
         {
             return View();
@@ -33,6 +36,26 @@ namespace ITintheDWebsite.Controllers
 
         public ActionResult Why()
         {
+            return View();
+        }
+
+        public ActionResult Apply()
+        {
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Apply(Application a)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Apps.Add(a);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
             return View();
         }
     }
